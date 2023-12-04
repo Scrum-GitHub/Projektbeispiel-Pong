b82ed156233a89e892fc860eca6c7c4f2408639e
aschwaiger74 <78038701+aschwaiger74@users.noreply.github.com>
2023-12-04 17:23:05 +0100

    #16 Startbildschirm

--- 01. Eingerichtet/main.cpp
+++ 01. Eingerichtet/main.cpp
@@ -5,8 +5,28 @@
 using namespace sf;
 
 int main()
-{
+{   
     
+    // Schriftart
+    Font font;
+    font.loadFromFile("fonts/arial.ttf");
+
+    // Label 
+    Text text("Start", font);
+    text.setCharacterSize(30);
+
+
+    // Spielefenster erstellen
+    RenderWindow meinSpieleFenster(VideoMode(800, 800), "Pong");
+    meinSpieleFenster.setFramerateLimit(30);
+
+    while (meinSpieleFenster.isOpen()) {
+
+       
+        meinSpieleFenster.clear();
+        meinSpieleFenster.draw(text);
+        meinSpieleFenster.display();
+    }
 
     return 0;
 }
